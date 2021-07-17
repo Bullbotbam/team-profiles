d8d2152ff3f83ed3e2c3488de2accc44630200d4
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-17 05:35:48 -0500

    Update page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -92,8 +92,8 @@ const generateHtml = (employeeCard) => {
 
 <body>
   <header>
-    <div class="container flex-row justify-space-between align-center py-3">
-      <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Profile</h1>
+    <div class="container justify-space-between align-center py-3">
+      <h1 class="page-title text-secondary py-2 px-3">Team Profile</h1>
      
     </div>
   </header>
